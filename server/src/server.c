8d10e77fce00e27ba63dccd4c7aaa7ba1c4ef8e2
Jan <jan@meznik.dk>
2014-02-18 09:23:12 +0100

    Added skeleton for multithreading

--- server/src/server.c
+++ server/src/server.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #include "double_list/list.h"
 #include "dbg.h"
@@ -23,39 +24,56 @@ void Server_open(Server *server, int port)
     server->listener_d = socket(PF_INET, SOCK_STREAM, 0);
     check(server->listener_d != -1, "Can't open socket");
     
+    server->server_lock = PTHREAD_MUTEX_INITIALIZER;    
+    server->running = 1;
+
+
     struct sockaddr_in name;
     name.sin_family = PF_INET;
     name.sin_port = (in_port_t)htons(port);
     name.sin_addr.s_addr = htonl(INADDR_ANY);
     int c = bind(server->listener_d, (struct sockaddr*)&name, sizeof(name));
     check(c!=-1, "Can't bind socket");
-    
-    // TODO: Create a new thread
-    Server_listen(server);
+
+    check(listen(server->listener_d, 10) != -1, "Can't listen");
+
+
+    // Create a new thread and start it
+    pthread_create(&server->listen_thread, 
+                    NULL, 
+                    Server_listen,
+                    (void*)server);
 
 error:
     return;
 }
 
-void Server_listen(Server *server)
-{ 
-     check(listen(server->listener_d, 10) != -1, "Can't listen");
- 
-   Client *c = client_create();
-   unsigned int address_size = sizeof(c->client_addr);
-    int foo = accept(server->listener_d, (struct sockaddr*)&c->client_addr, &address_size);
-    check(foo != -1, "Can't open client socket");
+void* Server_listen(Server *server)
+{
+        while(server->running)
+        { 
+                Client *c = client_create();
+                unsigned int address_size = sizeof(c->client_addr);
+                int foo = accept(server->listener_d, (struct sockaddr*)&c->client_addr, &address_size);
+                check(foo != -1, "Can't open client socket");
 
-    char *msg = "Connected...";
-    check(send(c->connect_d, msg, strlen(msg), 0) != -1, "Cannot send message");
-
+                char *msg = "Connected...";
+                check(send(c->connect_d, msg, strlen(msg), 0) != -1, "Cannot send message");
+        }
 error:
-    return;      
+        return;      
 }
 
 void Server_close(Server *server)
 {
+        void *result = NULL;
+        pthread_join(server->listen_thread, &result);
+
         List_clear_destroy(server->client_list);
+
+        // TODO: Close server
+        // TODO: Close(client_socket)
+
 
         free(server);
 }
--- server/src/server.h
+++ server/src/server.h
@@ -1,12 +1,18 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <pthread.h>
+
 #include "double_list/list.h"
 
 typedef struct{
         List *client_list;
         int port;
         int listener_d;
+       
+        volatile int running; 
+        pthread_t pthread;
+        pthread_mutex_t server_lock;        
 }Server;
 
 Server *Server_create();
